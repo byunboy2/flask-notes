6c5febbe42f457dd39fb8b71db900d62b6d3881d
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-25 14:00:07 -0700

    Finished part 3

--- app.py
+++ app.py
@@ -1,24 +1,77 @@
-from flask import Flask, render_template, redirect, flash
-
+from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-from models import db, connect_db, Pet
-
-import bycrypt
+from models import db, connect_db, User
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "secret"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///hashing_login"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
 
 connect_db(app)
 db.create_all()
 
-# Having the Debug Toolbar show redirects explicitly is often useful;
-# however, if you want to turn it off, you can uncomment this line:
-#
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
 toolbar = DebugToolbarExtension(app)
+
+@app.get("/")
+def redirect_to_register():
+    """Redirect to register page."""
+
+    return redirect("/register")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user: produce form & handle form submission."""
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        name = form.username.data
+        pwd = form.pwd.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User.register(name, pwd, email, first_name, last_name)
+        db.session.add(user)
+        db.session.commit()
+
+        session["username"] = user.username
+
+        # on successful registration, redirect to secret page
+        return redirect("/secret")
+
+    else:
+        return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Produce login form or handle login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        name = form.username.data
+        pwd = form.pwd.data
+
+        # authenticate will return a user or False
+        user = User.authenticate(name, pwd)
+
+    if user:
+        session["user_id"] = user.id  # keep logged in
+        return redirect("/secret")
+
+    else:
+        form.username.errors = ["Bad name/password"]
+
+    return render_template("login.html", form=form)
+
+@app.get("/secret")
+def successful_login():
+    """Return the text you made it"""
+
+    print("You made it")
+    return redirect("/")
---
+++ forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Length
+
+class RegisterForm(FlaskForm):
+    """Form for user registration"""
+
+    username = StringField('Username',
+                         validators=[InputRequired(),Length(max=20)]
+                            )
+    pwd = PasswordField("Password",
+                         validators=[InputRequired(),Length(max=100)])
+
+    email = EmailField("Email",
+                         validators=[InputRequired(),Length(max=50)])
+
+    first_name = StringField("First Name",
+                             validators=[InputRequired(),Length(max=30)])
+
+    last_name = StringField("Last Name",
+                             validators=[InputRequired(),Length(max=30)])
+
+
+class LoginForm(FlaskForm):
+    """Form for user login"""
+
+    username = StringField('Username',
+                         validators=[InputRequired(),Length(max=20)]
+                            )
+    pwd = PasswordField("Password",
+                         validators=[InputRequired(),Length(max=100)])
+
+
+class CSRFProtectForm(FlaskForm):
+    """Form just for CSRF Protection"""
